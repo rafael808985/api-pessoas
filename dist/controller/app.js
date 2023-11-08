5cabea57184c731923b98556ec6000444d399e5c
rafael808985 <126262710+rafael808985@users.noreply.github.com>
2023-11-08 03:46:03 -0300

    Atualização

--- dist/controller/app.js
+++ dist/controller/app.js
@@ -4,11 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const bancoDeDados_1 = require("./bancoDeDados");
+const Pessoa_1 = require("../model/Pessoa");
+const bancoDeDados_2 = require("./bancoDeDados");
 (0, bancoDeDados_1.inicializarPessoas)();
 const app = (0, express_1.default)();
-const port = 3001;
+const port = 9999;
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.get('/', (req, res) => {
     console.log('Recebi sua requisição');
     res.send({ mensagem: "Estou devolvendo a resposta para sua requisição" });
@@ -19,5 +23,18 @@ app.get('/pessoas', (req, res) => {
 });
 app.listen(port, () => {
     console.log(`Servidor Express ouvindo na endereço http://localhost:${port}/`);
+});
+// Rota para cadastrar uma pessoa
+app.post('/cadastro', (req, res) => {
+    // Recuperando as informações JSON que vieram no corpo (body) da requisição (req) e desestruturando essa informação para cada atributo
+    const { nome, cpf, data_nascimento, telefone, endereco, altura, peso } = req.body;
+    // Criando um novo objeto do tipo Pessoa com as informações recuperadas da requisição
+    const pessoa = new Pessoa_1.Pessoa(nome, cpf, new Date(data_nascimento), telefone, endereco, altura, peso);
+    // Apenas imprimindo as informações do objeto no console do servidor
+    console.log(pessoa);
+    // Chamando a função para persistir (salvar) os dados da pessoa no banco de dados
+    (0, bancoDeDados_2.persistirPessoa)(pessoa);
+    // Resposta que o servidor irá enviar ao front-end (A resposta será estrutura em um JSON)
+    res.json({ mensagem: "Pessoa cadastrada com sucesso" });
 });
 //# sourceMappingURL=app.js.map
--- src/controller/app.ts
+++ src/controller/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
+import cors from 'cors';
 import { inicializarPessoas, listarPessoas } from "./bancoDeDados";
+import { Pessoa } from "../model/Pessoa";
+import { persistirPessoa } from "./bancoDeDados";
 
 inicializarPessoas();
 
 const app = express();
-const port: number = 3001;
+const port: number = 9999;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     console.log('Recebi sua requisição');
@@ -23,3 +27,21 @@ app.get('/pessoas', (req, res) => {
 app.listen(port, () => {
     console.log(`Servidor Express ouvindo na endereço http://localhost:${port}/`);
 });
+
+// Rota para cadastrar uma pessoa
+app.post('/cadastro', (req, res) => {
+    // Recuperando as informações JSON que vieram no corpo (body) da requisição (req) e desestruturando essa informação para cada atributo
+const { nome, cpf, data_nascimento, telefone, endereco, altura, peso } = req.body;
+
+    // Criando um novo objeto do tipo Pessoa com as informações recuperadas da requisição
+const pessoa = new Pessoa(nome, cpf, new Date(data_nascimento), telefone, endereco, altura, peso);
+
+    // Apenas imprimindo as informações do objeto no console do servidor
+console.log(pessoa);
+
+    // Chamando a função para persistir (salvar) os dados da pessoa no banco de dados
+persistirPessoa(pessoa);
+
+    // Resposta que o servidor irá enviar ao front-end (A resposta será estrutura em um JSON)
+res.json({ mensagem: "Pessoa cadastrada com sucesso" })
+})
